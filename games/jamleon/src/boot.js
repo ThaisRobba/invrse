c375f3e969285c8b6780af5d80c86cffc13b0d13
OttoRobba <ottorobba@gmail.com>
2014-09-26 02:33:23 -0300

    css fixing

--- games/jamleon/src/boot.js
+++ games/jamleon/src/boot.js
@@ -1,22 +1,14 @@
 var bootState = {
-   preload: function() {
-      game.load.image('progressBar', 'art/ui/progress_bar.png');
-   },
-
-   create: function() {
-      game.physics.startSystem(Phaser.Physics.ARCADE);
-      game.stage.backgroundColor = '#3498db';
-      game.state.start('load');
-
-      game.global = {
-         mute: false
-      };
-
-   },
-
-   update: function() {
-
-   },
-
-
+	preload: function() {
+		game.load.image('progressBar', 'art/ui/progress_bar.png');
+	},
+	create: function() {
+		game.physics.startSystem(Phaser.Physics.ARCADE);
+		game.stage.backgroundColor = '#3498db';
+		game.state.start('load');
+		game.global = {
+			mute: false
+		};
+	},
+	update: function() {},
 };
